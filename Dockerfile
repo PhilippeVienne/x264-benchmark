366d08df1f80362d97ebf1df69cf9bcafe2a88e5
Philippe Vienne <philippe@skyloud.fr>
2024-07-25 09:34:05 +0200

    feat: use volume to add write performance

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM jrottenberg/ffmpeg:4.1-alpine
+FROM emmanuelgeo77ray/ffmpeg:6.1-ubuntu2004-libzimg
 
 RUN mkdir /input && mkdir /output
 COPY skydiver_10sec.mp4 /input
+ENV WRITE_FOLDER=/output
+VOLUME [ "/output" ]
 
 COPY entrypoint.sh /
 RUN chmod +x entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 CORES=`grep -c ^processor /proc/cpuinfo`
+WRITE_FOLDER=${WRITE_FOLDER:-/output}
+
+cp /input/skydiver_10sec.mp4 $WRITE_FOLDER/skydiver_10sec.mp4
+
+INPUT=$WRITE_FOLDER/skydiver_10sec.mp4
+OUTPUT=$WRITE_FOLDER/o-
 
 if [ -z ${THREADS+x} ]; then THREADS=$(($CORES / 2)); fi
 
@@ -13,10 +19,10 @@ do
     do
       printf "%10s: " ${preset}
       FRAMERATE_2X=$(($framerate * 2))
-      { time ffmpeg -hide_banner -loglevel quiet -i /input/skydiver_10sec.mp4 -c:v libx264 -profile:v high -s ${resolution} \
+      { time ffmpeg -hide_banner -loglevel quiet -i $INPUT -c:v libx264 -profile:v high -s ${resolution} \
         -preset ${preset} -b:v 8000K -bufsize 8000K -r ${framerate} -g ${FRAMERATE_2X} -keyint_min ${FRAMERATE_2X} \
         -sws_flags lanczos -pix_fmt yuv420p -f null -c:a aac -b:a 128k -strict normal -an -threads ${THREADS} \
-        - ; } 2> /output/${resolution}_${framerate}_${preset}.txt
+        -y ${OUTPUT}${resolution}_${framerate}_${preset}.mp4 2>&1 1>/dev/null; } 2> /output/${resolution}_${framerate}_${preset}.txt
       export SPEED=`cat /output/${resolution}_${framerate}_${preset}.txt | \
         head -n 1 | cut -f 2`
       printf "%4s%7s\n" ${SPEED}
